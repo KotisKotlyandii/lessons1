6f17a7fce29dc20687b1c3f0c2a521e92442dab9
Никита <nikita.iwanow.222@gmail.com>
2020-07-23 21:02:52 +0300

    Сделано 1 задание 2 урока(убрал лишние пробелы)

--- lesson_02/00_distance.py
+++ lesson_02/00_distance.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Есть словарь координат городов
-from pprint import  pprint
+from pprint import pprint
 
 sites = {
     'Moscow': (550, 370),
@@ -19,32 +19,25 @@ moscow = sites['Moscow']
 london = sites['London']
 paris = sites['Paris']
 
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** .5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
 
-moscow_london = ((moscow[0]-london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** .5
-moscow_paris = ((moscow[0]-paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
 
-distances ['Moscow'] = {}
-distances ['Moscow']['London'] = moscow_london
-distances  ['Moscow']['Paris'] = moscow_paris
-
-london_paris = ((london[0]-paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
-london_moscow = ((london[0]-moscow[0]) ** 2 + (london[1] - moscow[1]) ** 2) ** .5
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
+london_moscow = ((london[0] - moscow[0]) ** 2 + (london[1] - moscow[1]) ** 2) ** .5
 
 distances['London'] = {}
-distances  ['London']['Paris'] = london_paris
-distances  ['London']['Moscow'] = london_moscow
+distances['London']['Paris'] = london_paris
+distances['London']['Moscow'] = london_moscow
 
 paris_london = ((paris[0] - london[0]) ** 2 + (paris[1] - london[1]) ** 2) ** .5
 paris_moscow = ((paris[0] - moscow[0]) ** 2 + (paris[1] - moscow[1]) ** 2) ** .5
 
-
 distances['Paris'] = {}
-distances  ['Paris']['London'] = paris_london
-distances  ['Paris']['Moscow'] = paris_moscow
-
+distances['Paris']['London'] = paris_london
+distances['Paris']['Moscow'] = paris_moscow
 
 pprint(distances)
-
-
-
-
