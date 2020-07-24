f7679c6e34a4725db41622d81bd6d749230eb9ba
Никита <nikita.iwanow.222@gmail.com>
2020-07-24 22:27:06 +0300

    Cделано 7-ое задание 2-го урока

--- lesson_02/07_secret.py
+++ lesson_02/07_secret.py
@@ -20,5 +20,8 @@ secret_message = [
 #
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
-
-# TODO вывести расшифрованное сообщение
+print(secret_message[0][3])
+print(secret_message[1][9:13])
+print(secret_message[2][5:14:2])
+print(secret_message[3][-20:-26:-1])
+print(secret_message[4][-12:-17:-1])
