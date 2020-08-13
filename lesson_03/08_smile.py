b6583d0b22a581070c18cc0875872af101dee798
Никита <nikita.iwanow.222@gmail.com>
2020-08-13 14:41:31 +0300

    8-ое задание

--- lesson_03/08_smile.py
+++ lesson_03/08_smile.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
-
+import simple_draw as s
+from random import randint
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
+def head(x,y,color):
+    s.ellipse(left_bottom=x, right_top=y, color = color)
+for _ in range(10):
+    head(x = s.random_point(), y = s.random_point(), color=s.random_color())
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+s.pause()
