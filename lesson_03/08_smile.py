ded16fc4ea7376844b752a15e8c059851cb8284a
Никита <nikita.iwanow.222@gmail.com>
2020-08-14 15:59:47 +0300

    сделано 8-ое задание ( повтор )

--- lesson_03/08_smile.py
+++ lesson_03/08_smile.py
@@ -7,11 +7,16 @@ from random import randint
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
-def head(x,y,color):
-    s.ellipse(left_bottom=x, right_top=y, color = color)
+def smile(x, y, color):
+    radius = 50
+    s.circle(center_position=s.Point(x, y), radius=radius, color=color, width=0)
+    s.circle(center_position=s.Point(x + 15, y + 20), radius=7, color=s.COLOR_CYAN, width=0)
+    s.circle(center_position=s.Point(x - 15, y + 20), radius=7, color= s.COLOR_CYAN, width= 0)
+    s.line(start_point=s.Point(x - 20, y - 10), end_point=s.Point(x + 20, y - 10), color=s.COLOR_RED, width = 4)
+    s.line(start_point=s.Point(x -20 , y - 12), end_point=s.Point(x + -30, y + 5 ), color=s.COLOR_RED, width= 4)
+    s.line(start_point=s.Point(x + 20, y - 12), end_point=s.Point(x + 30, y + 5), color=s.COLOR_RED, width=4)
 for _ in range(10):
-
-    head(x = s.random_point(), y = s.random_point(), color=s.random_color())
-
+    smile(s.randint(0,600), s.randint(0,600), s.COLOR_YELLOW)
+s.pause()
 
 s.pause()
