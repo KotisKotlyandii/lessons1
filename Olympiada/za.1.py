4fc5245e4c2d87b57fb4bc0b4532bc9b38b084a0
liskos <liskosnik2011@yandex.ru>
2020-10-18 11:33:14 +0300

    подредактировал задание 1 олимпиады

--- Olympiada/za.1.py
+++ Olympiada/za.1.py
@@ -1,10 +1,16 @@
-1.
+import sys  # импорт системный модуль sys
+
+file = open(file="kodin.txt", mode="r")  # открываем файл kodin.txt для чтения read
+file2 = open(file="kodout.txt", mode="w")  # откываем файл  kodout.txt для записи write
+sys.stdin = file  # направляем поток ввода из файла file
+sys.stdout = file2  # направляем поток вывода в файл file2
+
 a = int(input())
 b = int(input())
 kol = 0
-for number in range(100,1000):
-        if number%10+number//100+(number//10) % 10 == a and number % b == 0:
-                print(number)
-                kol += 1
+for number in range(100, 1000):
+    if sum(map(int, str(number))) == a and number % b == 0:
+        print(number)
+        kol += 1
 if kol == 0:
-        print(0)
+    print(0)
