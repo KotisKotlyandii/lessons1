5f646f5871c093560fbdaded211c1c86325af8eb
Никита <nikita.iwanow.222@gmail.com>
2020-10-22 21:09:52 +0300

    Cделано 3-е задание 4-го урока

--- lesson_04/03_shape_select.py
+++ lesson_04/03_shape_select.py
@@ -8,7 +8,47 @@ import simple_draw as sd
 
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
+def triangle(*args):
+    common_part(*args,3)
 
-# TODO здесь ваш код
 
+def square(*args):
+    common_part(*args,4)
+
+
+def pentagon(*args):
+    common_part(*args,5)
+
+
+def hexagon(*args):
+    common_part(*args,6)
+
+
+def common_part(start_point,angle,length, kol_povt):
+    for _ in range(kol_povt):
+        start_point = sd.vector(start=start_point, angle=angle, length=length)
+        angle += 360/kol_povt
+
+
+a = int(input("""Возможные фигуры:
+0 - треугольник 
+1 - квадрат 
+2 - пятиугольник 
+3 - шестиугольник
+Введите желаемую фигуру > """))
+while True:
+    if -1 < a < 4:
+        break
+    else:
+        print("Введено некоректное число!")
+        a = int(input("Введите желаему фигуру > "))
+        continue
+if a == 0:
+    triangle(sd.Point(370, 100), 30, 100)
+if a == 1:
+    square(sd.Point(370, 100), 30, 100)
+if a == 2:
+    pentagon(sd.Point(150, 400), 30, 100, )
+if a == 3:
+    hexagon(sd.Point(150, 400), 30, 100, )
 sd.pause()
